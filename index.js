5f4dc3acfe8179dccc890cebca67ead4b1cafbeb
beyzabostanci <beyza_bostanc@hotmail.com>
2022-04-12 01:11:39 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+const names = ["Guadalupe", "Ollie", "Aki"]
+
+function writeCards(name){
+    let answer=[]
+    for (let i = 0; i<name.length; i++){
+        answer.push(`Thank you, ${name[i]}, for the wonderful surprise gift!`);
+        debugger;
+    }
+    return answer;
+}
+function countDown(){
+    var countdown = 10;
+   while (countdown > -1 ){
+    console.log(countdown--);
+}
+}
